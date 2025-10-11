2a0b1f616d41a351946e96773c9f60d7875591df
Geyger Andrey <gam301005@gmail.com>
2025-10-11 14:12:05 +0300

    new classes

--- models/users.py
+++ models/users.py
@@ -1,12 +1,23 @@
 
 from dataclasses import dataclass 
+from enum import Enum
+
+USER_ADULT_AGE = 18
+
+class Status(Enum):
+    STUDENT = "student"
+    WORKER = "worker"
 
 @dataclass
 class User : 
+    
     name : str
     age: int  
-    status : str
+    status : Status  
     items: list[str]
+
+    def is_adult(self)->bool: 
+        return self.age >= USER_ADULT_AGE
 
     # def __innit__(self,name,age,status,items):
     #     self.age = age 
--- test_newBW1.py
+++ test_newBW1.py
@@ -1,24 +1,30 @@
-import pytest , csv
+import pytest , csv 
+from models.users import User , USER_ADULT_AGE , Status
 
 @pytest.fixture
-def users(): 
+def users()->list[User]: 
     with open("users.csv") as file: 
         users = list(csv.DictReader(file,delimiter=';'))
-    return users 
+    return [User(
+        name=user["name"],
+        age=int(user["age"]),
+        status=Status(user["status"]),
+        items=user.get("items", "").split(",") if user.get("items") else []
+    ) for user in users]
 
 @pytest.fixture
-def workers(users):
+def workers(users) -> list[User]:
     workers = []
     for user in users: 
-        if user["status"] == "worker":
+        if user.status == Status.WORKER:
             workers.append(user)
 
     return workers
 
-def user_is_adult(user):
-    return int(user["age"]) >= 18
+def user_is_adult(user: User) -> bool:
+    return user.age >= USER_ADULT_AGE
 
 
 def test_worker_age(workers):
     for worker in workers:
-        assert user_is_adult(worker), f"worker {worker["name"]} under 18"
+        assert worker.is_adult(), f"worker {worker.name} under 18"
